d8414a6b2c1367acda982175e66866ddb08f511b
Jeffrey Ting <jting@bu.edu>
2024-07-02 16:44:30 -0500

    page to show leaderboard and buzzedin

---
+++ nextjs-frontend/app/both/page.tsx
@@ -0,0 +1,13 @@
+import Leaderboard from "@/components/leaderboard";
+import BuzzedIn from "@/components/buzzedIn";
+
+const BothPage = () => {
+	return (
+		<div className="flex justify-around">
+			<Leaderboard />
+			<BuzzedIn />
+		</div>
+	);
+};
+
+export default BothPage;
